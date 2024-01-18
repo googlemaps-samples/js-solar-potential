8056bceec0390ef88cb95da57b0e2993ab2e4738
David Cavazos <dcavazos@google.com>
2024-01-18 12:01:14 -0800

    include code coverage

---
+++ src/routes/solar.test.ts
@@ -0,0 +1,21 @@
+/*
+ copyright 2024 google llc
+
+ licensed under the apache license, version 2.0 (the "license");
+ you may not use this file except in compliance with the license.
+ you may obtain a copy of the license at
+
+      https://www.apache.org/licenses/license-2.0
+
+ unless required by applicable law or agreed to in writing, software
+ distributed under the license is distributed on an "as is" basis,
+ without warranties or conditions of any kind, either express or implied.
+ see the license for the specific language governing permissions and
+ limitations under the license.
+ */
+
+import { test } from 'vitest'
+import { } from './solar'
+
+test('import works', () => {
+})
